6a28305ec0faaf3145c3c74e49ce59afb11742bf
TusharNamdev <09tusharnamdev@gmail.com>
2025-11-13 12:20:08 +0530

    error resolve

--- .next/dev/static/chunks/app_testimonials_page_jsx_bca1c876._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_ecbf9293._.js",
-  "static/chunks/components_Testimonials_jsx_ee1ab857._.js"
-],
-    source: "dynamic"
-});
